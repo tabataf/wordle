2d448a3c7ca6308c4b1d7c36ffc4870175ff90c1
tabata Vasques <tabata07defaria@gmail.com>
2022-08-17 19:17:25 -0300

    tentando preencher a segunda coluna falhando

--- wordle.js
+++ wordle.js
@@ -4,6 +4,7 @@ console.log(linha1)
 let i = 0
 let lista = []
 let palavraTeste = ["a", 'm', 'o', 'r', 'a']
+respostas = []
 
 
 
@@ -18,18 +19,24 @@ function teclar(letra){
         console.log(lista)        
     }
 
-    else if(i == 30){
+    else if(i == 5){
         if(letra.keyCode == 8){
             i--
             lista.pop()
             linha1[i].innerHTML  = ''
         } else if(letra.keyCode=13){
-            lista.slice(0,5).toString()== palavraTeste.toString()
+            respostas.push(lista.slice(0,5).toString())
+            if(lista.slice(0,5).toString()== palavraTeste.toString()){
             alert ("você é bom!!")
+            }
+            else{
+                alert ("burrão!")
+                i++
+            }
+
 
             
-        }
-//            return 
+        } 
     } 
 
     else{
